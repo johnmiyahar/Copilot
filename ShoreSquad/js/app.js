98e0987e48b868c03c8255637c12608de4ff9f7f
johnmiyahar <23001571@myrp.edu.sg>
2025-06-02 14:55:04 +0800

    Initial commit for ShoreSquad project

---
+++ ShoreSquad/js/app.js
@@ -0,0 +1,7 @@
+// ShoreSquad JavaScript
+
+// Placeholder for map integration
+console.log('Map integration coming soon!');
+
+// Placeholder for weather API integration
+console.log('Weather API integration coming soon!');
